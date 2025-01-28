8b3760412ddd72ec84b50dfe23dd4fe4fd01c8b5
reggykoketso <168643546+reggykoketso@users.noreply.github.com>
2025-01-28 13:24:52 +0000

    885

--- app/src/main/java/com/example/hope/MainActivity.kt
+++ app/src/main/java/com/example/hope/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HopeTheme {
-        Greeting("Android")
+        Greeting("Androidoloolklkololk")
     }
 }
